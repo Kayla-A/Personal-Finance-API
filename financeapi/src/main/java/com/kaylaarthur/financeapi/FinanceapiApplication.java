6e3a31286227343aa669f9efcc49ad6655caf12d
Kayla-A <kayla.ka.arthur@outlook.com>
2026-03-30 21:39:59 -0400

    initial project setup

---
+++ financeapi/src/main/java/com/kaylaarthur/financeapi/FinanceapiApplication.java
@@ -0,0 +1,13 @@
+package com.kaylaarthur.financeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceapiApplication.class, args);
+	}
+
+}
